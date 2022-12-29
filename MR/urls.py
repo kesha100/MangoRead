843b516159342e47115ba73e3eb8a80d197ea9a1
kesha <arukeshxo.@gmail.com>
2022-12-29 20:07:42 +0600

    final touches

--- MR/urls.py
+++ MR/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/account/', include('author.urls')),
-    path('api/v1/catalog/', include('cards.urls'))
+    path('api/v1/collection/', include('cards.urls'))
 
 ]
 
